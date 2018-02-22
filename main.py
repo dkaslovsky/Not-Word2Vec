a916ec96ff9508a9442815d6427507e4e1e54294
dkaslovsky <dkaslovsky@gmail.com>
2018-02-21 21:21:17 -0700

    better construction of csr sparse matrix

--- main.py
+++ main.py
@@ -1,107 +1,44 @@
 """ Extremely initial implementation """
 
 import numpy as np
+import types
 
 from collections import Counter
 from itertools import chain, combinations, islice, izip, tee
-from scipy.sparse import dok_matrix
+from scipy.sparse import csr_matrix
 from scipy.sparse.linalg import svds
 
 from data import fetch_newsgroups_data, get_tokenized_docs
 
 
-
-class NormalizedCounter(Counter):
-
-    def normalize(self):
-        total = float(sum(self.itervalues()))
-        for key in self:
-            self[key] /= total
-
-
 class WordEmbedding(object):
 
-    def __init__(self, ngram_len):
+    def __init__(self, ngram_len, dim):
         self.ngram_len = ngram_len
+        self.dim = dim
         # populated by fit
-        self.docs = None
         self.vocab_ = None
         self.inv_vocab_ = None
-        self.unigram_prob_ = None
-        self.ngram_prob_ = None
+        self.vocab_len_ = None
         self.U_ = None
 
     def fit(self, docs):
-        # TODO: tee vs list, cache
-        # https://stackoverflow.com/questions/21315207/deep-copying-a-generator-in-python/21315536#21315536
-        # https://stackoverflow.com/questions/19503455/caching-a-generator
-        self.docs = list(docs)
-        self.unigram_prob_ = self.compute_unigram_probability()
-        self.ngram_prob_ = self.compute_ngram_probability()
-        # TODO: hashing trick?
-        self.vocab_ = {word: i for i, word in enumerate(sorted(self.unigram_prob_.iterkeys()))}
+        if isinstance(docs, types.GeneratorType):
+            # use list instead of itertools.tee since we will
+            # consume the entire generator before using it again
+            docs = list(docs)
+
+        unigram_counts = self.count_unigrams(docs)
+        ngram_counts = self.count_ngrams(docs)
+
+        self.vocab_ = {word: i for i, word in enumerate(sorted(unigram_counts.iterkeys()))}
         self.inv_vocab_ = {v: k for k, v in self.vocab_.iteritems()}
+        self.vocab_len_ = len(self.vocab_)
+
+        P = self.pmi_matrix(ngram_counts, unigram_counts)
+        self.U_ = self.embed(P, self.dim)
+
         return self
-
-    def compute_unigram_probability(self):
-        counts = self.count_unigrams()
-        counts.normalize()
-        return counts
-
-    def compute_ngram_probability(self):
-        counts = self.count_ngrams()
-        counts.normalize()
-        return counts
-
-    def count_unigrams(self):
-        return NormalizedCounter(chain.from_iterable(self.docs))
-
-    def count_ngrams(self):
-        counter = NormalizedCounter()
-        for doc in self.docs:
-            for ngram in self.ngram_generator(doc, self.ngram_len):
-                pairs = combinations(np.unique(ngram), 2)
-                counter.update(pairs)
-        return counter
-
-    # TODO: need to use adjustable size moving window?
-    @staticmethod
-    def ngram_generator(iterable, ngram_len):
-        return izip(*[islice(seq, i, None) for i, seq in enumerate(tee(iterable, ngram_len))])
-
-    def to_matrix(self):
-        n = len(self.vocab_)
-        S = dok_matrix((n, n), dtype=np.float32)
-        for pair, prob in self.ngram_prob_.iteritems():
-            S[self.to_index(pair)] = prob
-        S = S + S.T
-        return S
-
-    def to_array(self):
-        arr = np.empty((len(self.vocab_), 1))
-        for word, prob in self.unigram_prob_.iteritems():
-            arr[self.to_index(word)] = prob
-        return arr
-
-    def to_index(self, key):
-        if isinstance(key, tuple):
-            return self.vocab_[key[0]], self.vocab_[key[1]]
-        else:
-            return self.vocab_[key]
-
-    def pmi(self):
-        N = self.to_matrix()
-        U = self.to_array()
-        # TODO: make this sparse by masking U with indicator of N
-        U_U_inv = dok_matrix(1.0 / np.dot(U, U.T))
-        # TODO: should take a log here
-        return N.multiply(U_U_inv)
-
-    def embed(self, k):
-        pmi = self.pmi()
-        pmi.data = np.log(pmi.data)
-        U, _, _ = svds(pmi, k=k)
-        self.U_ = U
 
     def search(self, search_key):
         if isinstance(search_key, np.ndarray):
@@ -120,6 +57,63 @@ class WordEmbedding(object):
         idx = np.argsort(-np.dot(self.U_, vec))
         return [self.inv_vocab_[i] for i in idx[:3]]
 
+    @staticmethod
+    def embed(P, dim):
+        U, _, _ = svds(P, k=dim)
+        return U
+
+    def pmi_matrix(self, ngram_counts, unigram_counts):
+        # construct "adjacency" matrix of pairs from skipgrams
+        # TODO: check on definition as to whether this should be symmetric - we.to_index(sorted=True?)
+        data = ngram_counts.values()
+        row_idx, col_idx = zip(*[self.to_index(key, sort=True) for key in ngram_counts.iterkeys()])
+        P = csr_matrix((data, (row_idx, col_idx)), shape=(self.vocab_len_, self.vocab_len_))
+        P = P.multiply(1.0 / P.sum())  # normalize P to contain probabilities
+
+        # get inverse outer product matrix of unigram probabilities
+        # TODO: make this sparse by masking with indicator of P
+        uni_arr = self.to_array(unigram_counts)  # get array of unigram counts
+        uni_arr /= float(uni_arr.sum())  # normalize uni_arr to contain probabilities
+        U = 1.0 / np.dot(uni_arr, uni_arr.T)
+
+        # compute PMI matrix
+        P = P.multiply(csr_matrix(U))
+        P.data = np.log(P.data)
+        return P
+
+    @staticmethod
+    def count_unigrams(docs):
+        return Counter(chain.from_iterable(docs))
+
+    # TODO: symmetric or not?
+    def count_ngrams(self, docs):
+        counter = Counter()
+        for doc in docs:
+            for ngram in self.ngram_generator(doc, self.ngram_len):
+                pairs = combinations(np.unique(ngram), 2)
+                counter.update(pairs)
+        return counter
+
+    # TODO: need to use adjustable size moving window?
+    @staticmethod
+    def ngram_generator(iterable, ngram_len):
+        return izip(*[islice(seq, i, None) for i, seq in enumerate(tee(iterable, ngram_len))])
+
+    def to_index(self, key, sort=False):
+        if isinstance(key, tuple):
+            i1, i2 = self.vocab_[key[0]], self.vocab_[key[1]]
+            if sort:
+                i1, i2 = sorted([i1, i2])
+            return i1, i2
+        else:
+            return self.vocab_[key]
+
+    def to_array(self, count_dict):
+        arr = np.empty((self.vocab_len_, 1))
+        for word, prob in count_dict.iteritems():
+            arr[self.to_index(word)] = prob
+        return arr
+
 
 if __name__ == '__main__':
 
@@ -129,9 +123,7 @@ if __name__ == '__main__':
     data = fetch_newsgroups_data()
     docs = get_tokenized_docs(data[:100])
 
-    we = WordEmbedding(ngram_size)
+    we = WordEmbedding(ngram_size, dim)
     we = we.fit(docs)
-    we.embed(dim)
 
-    for word in we.vocab_.iterkeys():
-        print '%s -- %s' % (word, we.search(word))
+    print we.search('sportscenter')
