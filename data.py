a71757d92962d21382e04c0027b5662159b2565f
dkaslovsky <dkaslovsky@gmail.com>
2018-02-23 20:32:21 -0700

    class for cleaning documents

--- data.py
+++ data.py
@@ -27,3 +27,36 @@ def tokenize(word_iter):
 
 def get_tokenized_docs(docs):
     return (tokenize(doc.split()) for doc in docs)
+
+
+
+class Tokenizer(object):
+
+    def __init__(self):
+        self.regex = re.compile('^[a-z]+$')
+        self.chars_to_strip = CHARS_TO_STRIP
+        self.stop_words = STOP_WORDS
+
+    def tokenize(self, docs):
+        """
+
+        :param docs: iterable of documents (strings)
+        :return:
+        """
+        return (self._process_doc(doc.split()) for doc in docs)
+
+    def _process_doc(self, word_list):
+        words = self._clean(word_list)
+        words = self._remove_stop_words(words)
+        words = self._regex_filter(words)
+        return words
+
+    def _regex_filter(self, word_list):
+        return filter(self.regex.search, word_list)
+
+    def _remove_stop_words(self, word_list):
+        return filter(lambda x: x not in self.stop_words, word_list)
+
+    def _clean(self, word_list):
+        return (word.strip(self.chars_to_strip).replace('\'', '').lower()
+                for word in word_list)
