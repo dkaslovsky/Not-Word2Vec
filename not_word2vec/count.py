0f9be37d141b45592c0bbc6dcdd024257eaf40ee
dkaslovsky <dkaslovsky@gmail.com>
2018-02-27 07:24:13 -0700

    update tests and docstrings

--- not_word2vec/count.py
+++ not_word2vec/count.py
@@ -3,15 +3,22 @@ import types
 from collections import Counter
 
 
-# TODO: protect against single doc - ensure list of lists
-
 class WordCounter(object):
 
     def __init__(self, window_len):
+        """
+
+        :param window_len: int length of one side of skipgram window
+        """
         self.window_len = window_len
         self.skipgram_len = 2 * window_len + 1
 
     def count(self, docs):
+        """
+        Compute unigram and skipgram-pair counts
+        :param docs: iterable of lists where each list contains strings
+          in order of appearance in a document
+        """
         if isinstance(docs, types.GeneratorType):
             # use list instead of itertools.tee since we will
             # consume the entire generator before using it again
@@ -20,26 +27,49 @@ class WordCounter(object):
 
     @staticmethod
     def count_unigrams(docs):
+        """
+        Compute word counts
+        :param docs: iterable of lists where each list contains strings
+          in order of appearance in a document
+        """
         return Counter(it.chain.from_iterable(docs))
 
     def count_skipgram_pairs(self, docs):
+        """
+        Compute counts of pairs of the form (word i, word j)
+        :param docs: iterable of lists where each list contains strings
+          in order of appearance in a document
+        """
         counter = Counter()
         for doc in docs:
-            skipgrams = self.skipgram_generator(self.pad_document(doc))
-            pairs = [self.skipgram_to_pairs(skipgram) for skipgram in skipgrams]
+            skipgrams = self._skipgram_generator(self._pad_document(doc))
+            pairs = [self._skipgram_to_pairs(skipgram) for skipgram in skipgrams]
             counter.update(it.chain.from_iterable(pairs))
         return counter
 
-    def skipgram_to_pairs(self, skipgram):
+    def _skipgram_to_pairs(self, skipgram):
+        """
+        Construct list of (target, context) tuples where target is the center position
+         of the skipgram
+        :param skipgram: iterable/generator of strings of length 2 * self.window_len + 1
+        """
         target_word = skipgram[self.window_len]
         context_words = it.chain(it.islice(skipgram, 0, self.window_len),
                                  it.islice(skipgram, self.window_len + 1, None))
         return [(target_word, context_word) for context_word in context_words if context_word is not None]
 
-    def skipgram_generator(self, iterable):
-        return it.izip(*[it.islice(seq, i, None) for i, seq in enumerate(it.tee(iterable, self.skipgram_len))])
+    def _skipgram_generator(self, doc):
+        """
+        Construct generator of skipgrams
+        :param doc: list of strings
+        """
+        return it.izip(*[it.islice(seq, i, None) for i, seq in enumerate(it.tee(doc, self.skipgram_len))])
 
-    def pad_document(self, doc):
+    def _pad_document(self, doc):
+        """
+        Add self.window_len None values to the beginning and end of doc
+        :param doc: list of strings
+        """
         return it.chain(it.repeat(None, self.window_len),
                      doc,
                      it.repeat(None, self.window_len))
--- tests/test_count.py
+++ tests/test_count.py
@@ -3,48 +3,55 @@ import unittest
 from not_word2vec.count import WordCounter
 
 
-DOCS = [
-    'the quick brown fox jumps',
-    'over the lazy dog'
-]
-
-UNIGRAMS = {
-    'the': 2, 'quick': 1,
-    'brown': 1, 'fox': 1,
-    'jumps': 1, 'over': 1,
-    'lazy': 1, 'dog': 1,
-}
-
-NGRAMS = {
-    ('brown', 'quick'): 2, ('brown', 'fox'): 2, ('lazy', 'the'): 2,
-    ('dog', 'lazy'): 1, ('brown', 'the'): 1, ('dog', 'the'): 1,
-    ('quick', 'the'): 1, ('fox', 'quick'): 1, ('over', 'the'): 1,
-    ('brown', 'jumps'): 1, ('fox', 'jumps'): 1, ('lazy', 'over'): 1,
-}
-
-
-def docs_generator():
-    for doc in DOCS:
-        yield doc.split()
-
-
 class TestWordCounter(unittest.TestCase):
+    """
+    Unit tests for WordCounter
+    """
+
+    doc = 'a b c b e'.split()
+    expected_unigram_counts = {'a': 1, 'b': 2, 'c': 1, 'e': 1}
+    expected_pair_counts = {
+        ('a', 'b'): 1,
+        ('b', 'a'): 1,
+        ('b', 'c'): 2,
+        ('b', 'e'): 1,
+        ('c', 'b'): 2,
+        ('e', 'b'): 1
+    }
 
     def setUp(self):
-        ngram_len = 3
-        self.wc = WordCounter(ngram_len)
+        self.window_len = 1
+        self.wc = WordCounter(self.window_len)
+
+    def test_pad_document(self):
+        padded = self.wc._pad_document(self.doc)
+        expected_padded = [None] + self.doc + [None]
+        self.assertEqual(list(padded), expected_padded)
+
+    def test_skipgram_generator(self):
+        skipgrams = self.wc._skipgram_generator(self.doc)
+        self.assertEqual(skipgrams.next(), ('a', 'b', 'c'))
+        self.assertEqual(skipgrams.next(), ('b', 'c', 'b'))
+        self.assertEqual(skipgrams.next(), ('c', 'b', 'e'))
+        with self.assertRaises(StopIteration):
+            skipgrams.next()
+
+    def test_skipgram_to_pairs(self):
+        pairs = self.wc._skipgram_to_pairs(self.doc[:3])
+        expected_pairs = [('b', 'a'), ('b', 'c')]
+        self.assertEqual(pairs, expected_pairs)
+
+    def test_count_skipgram_pairs(self):
+        pair_counts = self.wc.count_skipgram_pairs([self.doc])
+        self.assertEqual(pair_counts, self.expected_pair_counts)
+
+    def test_count_unigrams(self):
+        unigram_counts = self.wc.count_unigrams([self.doc[:3], self.doc[3:]])
+        self.assertEqual(unigram_counts, self.expected_unigram_counts)
 
     def test_count(self):
-        docs = docs_generator()
-        unigrams, ngrams = self.wc.count(docs)
-        self.assertEqual(unigrams, UNIGRAMS)
-        self.assertEqual(ngrams, NGRAMS)
-
-    def test_ngram_generator(self):
-        doc = DOCS[0].split()
-        ngrams = self.wc.ngram_generator(doc)
-        self.assertEqual(ngrams.next(), ('the', 'quick', 'brown'))
-        self.assertEqual(ngrams.next(), ('quick', 'brown', 'fox'))
-        self.assertEqual(ngrams.next(), ('brown', 'fox', 'jumps'))
-        with self.assertRaises(StopIteration):
-            ngrams.next()
+        unigram_counts, skipgram_counts = self.wc.count([self.doc, self.doc])
+        expected_unigram_counts = {k: 2 * v for k, v in self.expected_unigram_counts.iteritems()}
+        expected_pair_counts = {k: 2 * v for k, v in self.expected_pair_counts.iteritems()}
+        self.assertEqual(unigram_counts, expected_unigram_counts)
+        self.assertEqual(skipgram_counts, expected_pair_counts)
